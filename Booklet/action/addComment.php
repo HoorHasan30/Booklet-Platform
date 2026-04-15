63be04a6d027ed65210e5ef80aff20fc4fc6db29
DanaK5 <202300513@student.polytechnic.bh>
2026-04-15 03:14:08 +0300

    fix success message color

--- Booklet/action/addComment.php
+++ Booklet/action/addComment.php
@@ -15,6 +15,6 @@ mysqli_query($dbc,"
 INSERT INTO dbProj_comments (commentText, reviewId, userId, createdAt)
 VALUES ('$commentText', $reviewId, $userId, CURDATE())
 ");
-
+$_SESSION['success'] = "Comment added successfully.";
 header("Location: ../reviewDetails.php?reviewId=$reviewId");
 ?>
