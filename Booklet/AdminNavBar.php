c7625e6c34882b68f4c4081db32121b2b5d6ac52
HoorHasan30 <202301820@student.polytechnic.bh>
2026-05-19 16:38:09 +0300

    UI Edits
    change navbar
    dd responsive css

--- Booklet/AdminNavBar.php
+++ Booklet/AdminNavBar.php
@@ -29,11 +29,10 @@
             <img class="logo" src="images/Logo.png" alt="Booklet Logo">
             
             <ul class="nav-links2">
-                <li><a href="Dashboard.php">Dashboard</a></li>
+                <li><a href="report_popular_content.php">Dashboard</a></li>
                 <li><a href="Home.php">Home</a></li>
                 <li><a href="AllBooks.php">All Books</a></li>
                 <li><a href="">All Users</a></li>
-                <li><a href="report_popular_content.php">Reports</a></li>
                 <li><a href="AboutUs.php">About Us</a></li>
                 
                 <li class="userInfo" onclick="toggleMenu()">🕮 <?php echo getUserInfo(); ?>
